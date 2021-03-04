48a0673f0aee6bae1ea19baa697289abc56ea9f8
Logan SQUIREL <logan.squirel@protonmail.com>
2021-03-04 12:13:07 +0100

    Add GitHub Action workflow

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
+pub fn one() -> u32 {
+    1
+}
+
+pub fn two() -> u32 {
+    2
+}
+
+pub fn three() -> u32 {
+    3
+}
+
 #[test]
 fn unit() {
-    assert_eq!(2 + 2, 4);
+    assert_eq!(one() + two(), three());
 }
